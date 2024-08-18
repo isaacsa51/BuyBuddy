8f4ff51ca6e906980863bc139b144d5e678d6aeb
Isaac Serrano <serranoie99@gmail.com>
2024-08-18 14:30:08 -0700

    HOTIFIX: Changed entity to model in UseCase

--- app/src/main/java/com/serranoie/android/buybuddy/domain/usecase/item/GetYearlySummaryBoughtUseCase.kt
+++ app/src/main/java/com/serranoie/android/buybuddy/domain/usecase/item/GetYearlySummaryBoughtUseCase.kt
@@ -1,8 +1,20 @@
 package com.serranoie.android.buybuddy.domain.usecase.item
 
 import com.serranoie.android.buybuddy.data.repository.ItemRepositoryImpl
+import com.serranoie.android.buybuddy.domain.model.MonthlySum
+import com.serranoie.android.buybuddy.domain.usecase.UseCaseResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetYearlySummaryBoughtUseCase @Inject constructor(private val itemRepositoryImpl: ItemRepositoryImpl) {
-
+    suspend fun invoke(): Flow<UseCaseResult<List<MonthlySum>>> = flow {
+        try {
+            val data = itemRepositoryImpl.getMonthlySumForItemsWithStatusOne().first()
+            emit(UseCaseResult.Success(data))
+        } catch (e: Exception) {
+            emit(UseCaseResult.Error(e))
+        }
+    }
 }
--- app/src/main/java/com/serranoie/android/buybuddy/domain/usecase/item/GetYearlySummaryToBuyUseCase.kt
+++ app/src/main/java/com/serranoie/android/buybuddy/domain/usecase/item/GetYearlySummaryToBuyUseCase.kt
@@ -2,6 +2,7 @@ package com.serranoie.android.buybuddy.domain.usecase.item
 
 import com.serranoie.android.buybuddy.data.persistance.entity.MonthlySumEntity
 import com.serranoie.android.buybuddy.data.repository.ItemRepositoryImpl
+import com.serranoie.android.buybuddy.domain.model.MonthlySum
 import com.serranoie.android.buybuddy.domain.usecase.UseCaseResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
@@ -9,10 +10,10 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetYearlySummaryToBuyUseCase @Inject constructor(private val itemRepositoryImpl: ItemRepositoryImpl) {
-    suspend operator fun invoke(): Flow<UseCaseResult<List<MonthlySumEntity>>> = flow {
+    suspend operator fun invoke(): Flow<UseCaseResult<List<MonthlySum>>> = flow {
         try {
-            val itemEntities = itemRepositoryImpl.getMonthlySumForItemsWithStatusZero().first()
-            emit(UseCaseResult.Success(itemEntities))
+            val data = itemRepositoryImpl.getMonthlySumForItemsWithStatusZero().first()
+            emit(UseCaseResult.Success(data))
         } catch (e: Exception) {
             emit(UseCaseResult.Error(e))
         }
